b8b6de10eb93af2e0ca0303ab36413e861042378
najafovmuhammad123-svg <najafovmuhammad123@gmail.com>
2026-09-11 15:23:33 +0400

    myProject

---
+++ codes/javase/SMC4builds/src/lesson1/hello.java
@@ -0,0 +1,16 @@
+package lesson1;
+
+public class hello {
+    public static void main(String[] args) {
+        int studentAge = 25;
+		double carPrice = 25000;	
+		String employeeName = "Malik";
+		char studentGrade = 'C';
+		boolean hasJob = false;
+		System.out.println(studentAge);	
+		System.out.println(carPrice);	
+		System.out.println(employeeName);
+		System.out.println(studentGrade);
+		System.out.println(hasJob);	
+	}
+}
