22360440a9ec8b572b30a39b58a753f5592617a7
najafovmuhammad123-svg <najafovmuhammad123@gmail.com>
2026-09-19 15:01:42 +0400

    blah
    hur

---
+++ codes/javase/SMC4builds/src/lesson1/PrimitiveVar.java
@@ -0,0 +1,15 @@
+package lesson1;
+
+public class PrimitiveVar {
+	public static void main(String[] args) {
+		int a = 25;
+		double b = 19.99d;
+		char c = 'A';
+		boolean d = true;
+		  
+	        System.out.println(a);
+	        System.out.println(b);
+	        System.out.println(c);
+	        System.out.println(d);
+	}
+}
--- codes/javase/SMC4builds/src/lesson1/VariablesPracticeHome.java
+++ codes/javase/SMC4builds/src/lesson1/VariablesPracticeHome.java
@@ -2,15 +2,6 @@ package lesson1;
 
 public class VariablesPracticeHome {
     public static void main(String[] args) {
-        int studentAge = 25;
-		double carPrice = 25000;	
-		String employeeName = "Malik";
-		char studentGrade = 'C';
-		boolean hasJob = false;
-		System.out.println(studentAge);	
-		System.out.println(carPrice);	
-		System.out.println(employeeName);
-		System.out.println(studentGrade);
-		System.out.println(hasJob);	
+  int a = 10, b = 40, c;
 	}
 }
